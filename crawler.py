56db1a30f603c5cd1525666958b240e1d12e5bfb
mcsgit-sketch <mcsgit@gmail.com>
2026-09-04 17:49:52 +0400

    Make fixes to crawler

--- crawler.py
+++ crawler.py
@@ -1,15 +1,18 @@
 from urllib.parse import urlsplit, urljoin, urlparse, urldefrag
-import sqlite3
-from parser import get_html, get_title, parse_html
+from parser import get_html, get_title, parse_html, get_text
 from storage import create_db, store_page
+from collections import deque
 
+# url queue
+urls = deque(["https://www.nytimes.com/", "https://www.bbc.com/", 
+        "https://www.reddit.com/", "https://www.ycombinator.com/", 
+        "https://www.usa.gov/", "https://github.com/", 
+        "https://www.reuters.com/", 
+        "https://en.wikipedia.org/wiki/Main_Page"])
 
-# global variables 
-urls = set() 
-
-
-# function to retrive page html
-url = "https://en.wikipedia.org/wiki/Machine_learning"
+#store seen (visited) links
+# can this be done by adding a "visited?" column in the database to have the memory persist
+seen = set()
 
 def get_links(soup, base_url): 
     links = set() 
@@ -21,15 +24,61 @@ def get_links(soup, base_url):
         link = urljoin(base_url, link)
         link, _ = urldefrag(link)
         
-        parsed = urlparse(link)
-        if parsed.scheme not in ("http", "https"):
+        if not should_crawl(link): 
             continue
-
         links.add(link)
     return links
 
+
+def should_crawl(url): 
+    parsed = urlparse(url)
+    if parsed.scheme not in ("http", "https"):
+        return False 
+
+    blocked_extensions = (".jpg", ".jpeg", ".png", ".gif", 
+                          ".pdf", ".zip", ".mp4", ".mp3")
+    
+    if parsed.path.lower().endswith(blocked_extensions): 
+        return False 
+
+    return True 
+
+def crawl(url):
+    html = get_html(url)
+    if html is None: 
+        return False 
+
+    soup = parse_html(html)
+    
+    title = get_title(soup)
+    if title == "":
+        return False
+    
+    text = get_text(soup)
+    
+    for link in get_links(soup, url):
+        if link not in seen: 
+            urls.append(link)
+    seen.add(url)
+    store_page(url, title, text)
+
+    return True
+
+def crawling(limit): 
+    crawl_count = 0
+    while urls and crawl_count < limit:
+        current_url = urls.popleft()
+        if current_url in seen: 
+            continue
+
+        if crawl(current_url): 
+            crawl_count = crawl_count + 1
+    return "success"
+
 def main(): 
-    print(get_links(get_html(url), url))
+    #    print(get_links(get_html(url), url))
+    create_db()
+    print(crawling(10))
 
 
 
